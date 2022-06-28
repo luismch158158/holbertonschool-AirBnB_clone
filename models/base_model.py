54b719bf702052ee7d324171bfa8047329160a62
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-27 19:05:51 -0500

    Fix pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
+"""Base Model """
 
 import uuid
 from datetime import datetime
 
-"""Base Model """
+
 class BaseModel():
     """class BaseModel that defines all common attributes/methods
         for other classes """
     def __init__(self):
-        self.id=str(uuid.uuid4())
-        self.created_at=datetime.now()
-        self.updated_at=datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        update_at=datetime.now()
+        update_at = datetime.now()
 
     def to_dict(self):
         return ({
             'my_number': self.my_number,
             'name': self.name,
             '__class__': self.__class__.__name__,
-            'updated_at': self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f") ,
+            'updated_at': self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
             'id': self.id,
             'created_at': self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"),
             })
