5f46a6821cf9404ebf0f76b73b9843862df8faad
LesLor <leslie.paz.am@gmail.com>
2022-06-28 23:05:25 +0000

    feet: adding a dictionary and import

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
+dictionaries={"BaseModel":BaseModel}
 storage = FileStorage()
 storage.reload()
