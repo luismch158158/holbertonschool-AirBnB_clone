7363e09b8df03701b82d350d9c795053d826caec
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-28 14:20:27 -0500

    Feat: Adding file storage, serialize and desarialize

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 new_date = "%Y-%m-%dT%H:%M:%S.%f"
@@ -14,8 +15,8 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if  kwargs is not None:
-
+        if  kwargs is not None and len(kwargs) != 0:
+            print("estoy en kwargs")
             for key in kwargs.keys():
                 if key != '__class__':
                     setattr(self, key, kwargs[key])
@@ -27,13 +28,15 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        update_at = datetime.now()
+        self.update_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         return ({
