b124880126ae7339a457513d8b8cbc8facf4be3d
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-29 17:48:49 -0500

    Feat: Implemented class amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+import models
+
+class Amenity(BaseModel):
+    """
+    Class Amenity that inherits from BaseModel
+    """
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes Amenity"""
+        super().__init__(*args, **kwargs)
