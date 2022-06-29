eb07359dd3cc5e3a73968e52c8dc7843eace3fae
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-29 17:44:21 -0500

    Feat: importing all classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,13 @@ import json
 from models.base_model import BaseModel
 from os.path import exists
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+
+
 
 """
 
