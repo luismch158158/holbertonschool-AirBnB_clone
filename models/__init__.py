e61f68a4cff83b0f211130f22d9d00c7b820b4ae
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-29 15:28:22 -0500

    Feat: Adding classes to dictionary

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
 
-dictionaries={"BaseModel":BaseModel}
+dictionaries = {"BaseModel": BaseModel, "User": User, "Place": Place, "State": State,"City": City, "Amenity": Amenity }
+
 storage = FileStorage()
 storage.reload()
