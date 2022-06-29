54e7557811be3956763e5447a9a51cd2c6b662cb
LesLor <leslie.paz.am@gmail.com>
2022-06-29 00:01:19 +0000

    feet: first -v of console and create command

--- console.py
+++ console.py
@@ -2,27 +2,36 @@
 
 import cmd
 import string, sys
+from models.base_model import BaseModel
 
-class CLI(cmd.Cmd):
+class HBNBCommand(cmd.Cmd):
+
+    list_class = ["BaseModel"]
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt ='(hbnb)'
-
-    def help_hello(self):
-        print ("syntax: hello [message]")
-        print ("__ prints a hello message")
 
     def do_quit(self, arg):
         sys.exit(1)
 
     def do_EOF(self, line):
+        """End Of File"""
+        print()
         return True
 
+    def do_create(self, line):
+        if line == "" or None:
+            print("** class name missing **")
+        elif line is not in list_class:
+            print("** class doesn't exist **")
+        else:
+            instance = eval(f'{line}()')
+            instance.save()
+            print(instance.id)
+
     def help_quit(self):
-        print ("syntax: quit",)
-        print ("__ terminates the application")
+        print ("Quit command to exit the program")
 
-    do_q = do_quit
 
-cli = CLI()
-cli.cmdloop()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
