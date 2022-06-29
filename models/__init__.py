f954b4ee59ae789621a0968d388f32e559c158ce
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-29 17:48:15 -0500

    Feat: importing all review adding new class in the dictionaries

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.review import Review
 from models.user import User
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.state import State
 
-dictionaries = {"BaseModel": BaseModel, "User": User, "Place": Place, "State": State,"City": City, "Amenity": Amenity }
+dictionaries = {"BaseModel": BaseModel, "User": User, "Place": Place, "State": State,"City": City, "Amenity": Amenity, "Review": Review }
 
 storage = FileStorage()
 storage.reload()
