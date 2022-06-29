0073fe4836cfc2eaba972a53ac751b8114c1021b
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-29 17:52:51 -0500

    Feat: Implemented class state

--- models/state.py
+++ models/state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+import models
+
+class State(BaseModel):
+    """
+    Class State that inherits from BaseModel
+    """
+
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes state"""
+        super().__init__(*args, **kwargs)
