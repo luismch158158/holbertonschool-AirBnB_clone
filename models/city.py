7671fe3fb4b629cd106a5a3071c46f05b4898913
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-06-29 17:51:26 -0500

    Feat: Implemented class city

--- models/city.py
+++ models/city.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+import models
+
+class City(BaseModel):
+    """
+    Class City that inherits from BaseModel
+    """
+
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes City"""
+        super().__init__(*args, **kwargs)
