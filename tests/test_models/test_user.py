9bb8bc73325c8bd0a76ff0dfb3a4329de57050aa
LesLor <leslie.paz.am@gmail.com>
2022-07-01 19:06:16 +0000

    Feat: adding test for User Class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,14 +1,25 @@
 #/usr/bin/python3
 
 import unittest
+from models.user import User
 
 class TestUser(unittest.TestCase):
 
-    def test_user_atributes(self):
-        pass
+    def setUp(self):
+        self.user = User()
 
     def test_user_instantiation(self):
-        pass
+        #duda
+        self.assertIsInstance(self.user, User)
+
+    def test_user_atributes(self):
+        self.assertTrue(type(self.id) is int), 
+        self.assertTrue(type(self.created_at) is datetime), 
+        self.assertTrue(type(self.updated_at) is datetime), 
+        self.assertTrue(type(self.email) is str), 
+        self.assertTrue(type(self.password) is str), 
+        self.assertTrue(type(self.first_name) is str), 
+        self.assertTrue(type(self.last_name) is str), 
 
     def test_user_save(self):
         pass
@@ -19,6 +30,8 @@ class TestUser(unittest.TestCase):
     def test_user__str__(self):
         pass
 
+    def tearDown(self):
+        del self.user      
 
 if __name__ == '__main__':
     unittest.main()
