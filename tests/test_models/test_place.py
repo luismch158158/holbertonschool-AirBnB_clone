6a98dffa8dfcee2370dcfa4c0b726d91e5d3fe70
LesLor <leslie.paz.am@gmail.com>
2022-07-01 21:41:22 +0000

    Feat: adding unnitest for place and User

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,13 +1,26 @@
 #/usr/bin/python3
 
+from datetime import datetime
+import json
 import unittest
+import uuid
+from models.place import Place
 
 class TestPlace(unittest.TestCase):
     """
     """
+    def setUp(self):
+        self.place = Place()
 
-    def test_place_atributes(self):
-        pass
+    def test_place_atributes_id(self):
+        self.assertTrue(type(self.place.id) is str)
+
+    def test_place_created_at(self):
+        self.assertTrue(type(self.place.created_at) is datetime)
+        self.assertTrue(type(self.place.updated_at) is datetime)
+        self.assertTrue(type(self.place.city_id) is str)
+        self.assertTrue(type(self.place.user_id) is str)
+
 
     def test_place_instantiation(self):
         pass
@@ -21,6 +34,8 @@ class TestPlace(unittest.TestCase):
     def test_place__str__(self):
         pass
 
+    def tearDown(self):
+        del self.place      
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,58 @@
 #/usr/bin/python3
-
+from datetime import datetime
+import json
 import unittest
+import uuid
 from models.user import User
+import os
+import models
 
 class TestUser(unittest.TestCase):
 
     def setUp(self):
         self.user = User()
+        self.user.first_name = "Leslie"
+        self.user.last_name = "Paz"
+        self.user.password = "***************"
+        self.user.email = "prueba@gmail.com"
+        self.user.save()
 
     def test_user_instantiation(self):
-        #duda
         self.assertIsInstance(self.user, User)
 
-    def test_user_atributes(self):
-        self.assertTrue(type(self.id) is int), 
-        self.assertTrue(type(self.created_at) is datetime), 
-        self.assertTrue(type(self.updated_at) is datetime), 
-        self.assertTrue(type(self.email) is str), 
-        self.assertTrue(type(self.password) is str), 
-        self.assertTrue(type(self.first_name) is str), 
-        self.assertTrue(type(self.last_name) is str), 
+    def test_user_atributes_id(self):
+        self.assertTrue(type(self.user.id) is str)
+
+    def test_user_atributes_created_at(self):
+        self.assertTrue(type(self.user.created_at) is datetime)
+
+    def test_user_atributes_update_at(self):
+        self.assertTrue(type(self.user.updated_at) is datetime)
+
+    def test_user_atributes_email(self):
+        self.assertTrue(type(self.user.email) is str)
+
+    def test_user_atributes_password(self):
+        self.assertTrue(type(self.user.password) is str)
+
+    def test_user_atributes_first_name(self):
+        self.assertTrue(type(self.user.first_name) is str)
+
+    def test_user_atributes_last_name(self):
+        self.assertTrue(type(self.user.last_name) is str) 
 
     def test_user_save(self):
-        pass
+        PATH = './file.json'
+        value = os.path.isfile(PATH)
+        self.assertTrue(value)
 
     def test_user_to_dict(self):
-        pass
+        self.assertTrue(type(self.user.to_dict()) is dict)
 
     def test_user__str__(self):
-        pass
+        name = self.user.__class__.__name__
+        user = f'[{name}] ({self.user.id}) {self.user.__dict__}'
+        self.assertTrue(self.user.__str__() == user)
 
     def tearDown(self):
         del self.user      
