88b7168c27070bd59a598c4e6a9876b57fab15fb
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-02 11:23:40 -0500

    Fix removing first initializing in constructor

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,9 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Constructor of BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        # self.id = str(uuid.uuid4())
+        # self.created_at = datetime.now()
+        # self.updated_at = datetime.now()
         if kwargs is not None and len(kwargs) != 0:
             for key in kwargs.keys():
                 if key != '__class__':
