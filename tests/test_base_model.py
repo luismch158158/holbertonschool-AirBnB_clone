572ea49e0674db68a2726d05ba00881ed90540ca
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-02 12:48:24 -0500

    Moving in other directory

---
+++ tests/test_base_model.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Module that describe the BaseModel test class"""
+from datetime import datetime
+import json
+import unittest
+import uuid
+from models.base_model import BaseModel
+import os
+import models
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+        Class for test the BaseModel Class method and atributes
+    """
+
+    def setUp(self):
+        """All variables that we need
+        to start testing BaseModel class"""
+        self.basemodel = BaseModel()
+        self.basemodel.save()
+
+    def test_basemodel_instantiation(self):
+        """Test User instantiation"""
+        self.assertIsInstance(self.basemodel, BaseModel)
+
+    def test_basemodel_atributes_id(self):
+        """Test User atribue id"""
+        self.assertTrue(type(self.basemodel.id) is str)
+
+    def test_basemodel_atributes_created_at(self):
+        """Test BaseModel atribue created at"""
+        self.assertTrue(type(self.basemodel.created_at) is datetime)
+
+    def test_basemodel_atributes_update_at(self):
+        """Test Basemodel atribue update_at"""
+        self.assertTrue(type(self.basemodel.updated_at) is datetime)
+
+    def test_basemodel_save(self):
+        """Test if the instance is stored correctly"""
+        PATH = './file.json'
+        value = os.path.isfile(PATH)
+        self.assertTrue(value)
+
+    def test_basemodel_to_dict(self):
+        """Test if the basemodel dict has the correct format"""
+        self.assertTrue(type(self.basemodel.to_dict()) is dict)
+
+    def test_basemodel__str__(self):
+        """Test if the basemodel ___str__ method has the correct format
+            descrited in the method"""
+        name = self.basemodel.__class__.__name__
+        basemodel = f'[{name}] ({self.basemodel.id}) {self.basemodel.__dict__}'
+        self.assertTrue(self.basemodel.__str__() == basemodel)
+
+    def tearDown(self):
+        """Closing the test class and deleteing the instance
+        created for testing"""
+        del self.basemodel
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_state.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Module that describe the State test class"""
+from datetime import datetime
+import json
+import unittest
+import uuid
+from models import state
+from models.state import State
+import os
+import models
+
+
+class TestReview(unittest.TestCase):
+    """Test case class of unittest"""
+
+    def setUp(self):
+        """Method that sets all initial values"""
+        self.state = State()
+        self.state.name = "San Francisco"
+
+    def test_state_instantiation(self):
+        """Test to validation instantiation of State"""
+        self.assertIsInstance(self.state, State)
+
+    def test_state_atributes_id(self):
+        """Test to validation datatype of id"""
+        self.assertTrue(type(self.state.id) is str)
+
+    def test_state_atributes_created_at(self):
+        """Test to validation datatype of created_at"""
+        self.assertTrue(type(self.state.created_at) is datetime)
+
+    def test_state_atributes_update_at(self):
+        """Test to validation datatype of updated_at"""
+        self.assertTrue(type(self.state.updated_at) is datetime)
+
+    def test_state_name(self):
+        """Test to validation datatype of name"""
+        self.assertTrue(type(self.state.name) is str)
+
+    def test_state_save(self):
+        """Test to validation if I successufully create
+        the file.json"""
+        PATH = './file.json'
+        value = os.path.isfile(PATH)
+        self.assertTrue(value)
+
+    def test_state_to_dict(self):
+        """Test to validation if method dict return data type dict"""
+        self.assertTrue(type(self.state.to_dict()) is dict)
+
+    def test_state__str__(self):
+        """Test to validation if method str return correct format"""
+        name = self.state.__class__.__name__
+        state = f'[{name}] ({self.state.id}) {self.state.__dict__}'
+        self.assertTrue(self.state.__str__() == state)
+
+    def tearDown(self):
+        """Method to close tests"""
+        del self.state
+
+
+if __name__ == '__main__':
+    unittest.main()
