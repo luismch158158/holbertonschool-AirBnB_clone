28f59fc7ccf20332e9f8ede6a3518f203095e576
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-07-02 10:13:42 -0500

    Adding model of test_file_storage

--- tests/test_models/tests_engine/test_file_storage.py
+++ tests/test_models/tests_engine/test_file_storage.py
@@ -2,3 +2,22 @@
 """Module that describe the FileStorage test class"""
 
 import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+        Class for test the BaseModel Class method and atributes
+    """
+
+    def setUp(self):
+        """All variables that we need"""
+        pass
+
+    def tearDown(self):
+        """Closing the test class and deleteing the instance
+            created for testing"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
